7e6128f72edab79fd7ee4f03aca760c71453e27f
karma <karma.rout@gmail.com>
2025-09-12 00:44:00 +0530

    dist removed

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,3 +1,3 @@
 window._env_ = {
-  API_URL: "http://localhost:4000", // 👈 change to your backend URL when deploying
+  API_URL: "https://cscenter-client-mgmt-6.onrender.com", // 👈 change to your backend URL when deploying
 };
